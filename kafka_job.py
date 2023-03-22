aa7a808c75d1d7937505e2349ed311a333880c09
ShreyasK2411 <114290670+ShreyasK2411@users.noreply.github.com>
2023-03-22 20:02:04 +0530

    Update kafka_job.py

--- kafka_job.py
+++ kafka_job.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 # setting the default arguments
 default_args = {
     'owner':'Shreyas',
-    'start_date':datetime(2023,3,21,13), # starting from today at 3pm
+    'start_date':datetime(2023,3,21,15), # starting from today at 3pm
     'retries':3,
     'retry_delay':timedelta(minutes=5) # if failed retry after every 5 mins
 }
@@ -14,7 +14,7 @@ dag = DAG(
     dag_id = "Kafka_Streaming_Job",
     default_args = default_args,
     description = "Stream daily stocks data using kafka and put it in database.",
-    schedule_interval= '57 13 * * 1-5', # schedule everyday at 3pm
+    schedule_interval= '0 15 * * 1-5', # schedule everyday at 3pm
     catchup = True
 )
 
